33abd13e0d1d348360f639fefd17fe2af859142e
Bartosz jaje <79916486+jajszu@users.noreply.github.com>
2022-03-18 13:13:53 +0100

    Gloving Menu
    Gloving side menu in js and css

---
+++ index.js
@@ -0,0 +1,8 @@
+function navigation(a){
+  a.classList.toggle("active-nav");
+  a.children[1].classList.toggle("nav-a");
+  a.children[2].classList.toggle("nav-b");
+  a.children[3].classList.toggle("nav-c");
+  a.children[4].classList.toggle("nav-d");
+  a.children[5].classList.toggle("nav-e");
+}
